59ac53dbb36e958e9ec5d3eb219a38d9e6b08ede
nichengzhi <nichengzhi34@gmail.com>
2018-12-20 18:37:20 -0500

    add begin scratch

---
+++ additems.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User,Category,Item
+
+engine = create_engine("sqlite:///categoryitem.db")
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+# Create dummy user
+User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+session.add(User1)
+session.commit()
+
+# Menu for UrbanBurger
+categories = {"Soccer" : ["Shinguards", "Two Shinguards", "Jersey", "Soccer Cleats"], 
+            "Basketball":[], 
+            "Baseball":["Bat"], 
+            "Frisbee" : ["Friebee"], 
+            "Snowboarding" : ["Snownboard", "Goggles"],
+              "Rock Climbing" : [], 
+              "Foosball" : [], 
+              "Skating" : [], 
+              "Hockey" : ["Stick"]}
+for category in categories.keys():
+    cat = Category(user_id = 1, name = category)
+    session.add(cat)
+    for item in categories[category]:
+
+        item = Item(user_id = 1, name = item, category = cat, description = "need update")
+        session.add(item)
+    session.commit()
+
+
---
+++ database_setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import datetime
+ 
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+class Category(Base):
+
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    @property
+    def serialize(self):
+        
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'id'           : self.id,
+       }
+
+class Item(Base):
+
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250))
+    created_date = Column(DateTime, default=datetime.datetime.utcnow)
+    catagory_id = Column(Integer,ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'name'         : self.name,
+           'description'         : self.description,
+           'id'         : self.id,
+           'create_date' : self.created_date
+       }
+
+engine = create_engine('sqlite:///categoryitem.db')
+
+Base.metadata.create_all(engine)
